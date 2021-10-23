0ee99a9e375018a201748c03a8d64c6129daf20d
GOKUL-EASWARAN <81260382+GOKUL-EASWARAN@users.noreply.github.com>
2021-10-23 16:55:58 +0530

    change

--- app2.js
+++ app2.js
@@ -1,11 +1,12 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-gsap.utils.toArray(".panel").forEach((panel,i)=>{
-    ScrollTrigger.create({
-        trigger:panel,
-        start:"top top",
-        pin: true,
-        markers:true,
-        pinSpacing:false
-    })
-})
+// gsap.utils.toArray(".panel").forEach((panel,i)=>{
+//     ScrollTrigger.create({
+//         trigger:panel,
+//         start:"top top",
+//         pin: true,
+//         markers:true,
+//         pinSpacing:false
+//     })
+   
+// })
